710e7fa53360ff04eda2a8a2c4a110a94104e1eb
Prasidda Pokhreal <prasiddha@Prasiddhas-MacBook-Air.local>
2025-04-23 07:55:57 +0545

    changed teraform

--- teraform.tf
+++ teraform.tf
@@ -27,19 +27,7 @@ resource "aws_security_group" "my_tc_security" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   
-#   ingress {
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
 
-#   ingress {
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
 
   egress {
     from_port   = 0
